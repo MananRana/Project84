6e07c920615aeebff09679b7ee7d176ac20aed78
MananRana <87970269+MananRana@users.noreply.github.com>
2022-01-08 11:13:36 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Create a reference for the canvas
-
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
 img_width = 300;
 img_height = 100;
 
@@ -20,7 +21,7 @@ function uploadimg() {
 }
 
 //Write a code to grab the key-pressed event
-
+window.addEventListener("keydown",my_keydown);
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
@@ -28,6 +29,26 @@ function my_keydown(e)
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
 		//write a code to check the type of key pressed
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed alphabet key";
+			console.log("alphabetkey");
+		}
+		else if(keyPressed>=48&&keyPressed<=57){
+		numberkey();
+			document.getElementById("d1").innerHTML="You pressed number key";
+			console.log("numberkey");	
+		}
+		else if(keyPressed>=37&&keyPressed<=40){
+			arrowkey();
+				document.getElementById("d1").innerHTML="You pressed arrow key";
+				console.log("arrowkey");	
+			}
+			else if((keyPressed==17)||(keyPressed==18||keyPressed==27)){
+				specialkey();
+					document.getElementById("d1").innerHTML="You pressed ctrl,esc,alt key";
+					console.log("specialkey");	
+				}
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
@@ -37,18 +58,23 @@ function my_keydown(e)
 function aplhabetkey()
 {
 	//upload respective image with the message. 
-
+img_image="Alpkey.png";
+add();
 }
 function numberkey()
 {
-	
+	img_image="numkey.png";
+	add();	
 }
 function arrowkey()
 {
+	img_image="Arrkey.png";
+add();
 }
 function specialkey()
 {
-	
+	img_image="spkey.png";
+	add();	
 }
 function otherkey()
 {
